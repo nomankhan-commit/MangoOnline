63a5524c0385f11bf323183c950c96750c908885
NOMI NOMI <nhr146@outlook.com>
2025-01-06 05:06:40 +0500

    -> Create coupon list and get by id APIs. -> Add common response. -> Add apply migration function in program.cs

---
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,55 @@
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private ResponseDto _responseDto;
+        public CouponAPIController(AppDbContext db)
+        {
+            _db = db;
+            _responseDto = new ResponseDto();
+        }
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                 IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                 _responseDto.Result = objList;
+            }
+            catch (Exception ex)
+            {
+
+                _responseDto.IsSuccess = false;
+                _responseDto.Result = ex.Message;
+
+            }
+            return _responseDto;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public object Get(int id) {
+
+            try
+            {
+                Coupon objList = _db.Coupons.First(u=>u.CouponId==id);
+                _responseDto.Result = objList;
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Result = ex.Message;
+            }
+            return _responseDto;
+        }
+    }
+}
