4cc73601b75f04a75a4f7ee8054368bf8aef4bbb
NOMI NOMI <nhr146@outlook.com>
2025-01-08 05:47:35 +0500

    Add coupon create functionality.

--- Mango.Web/Controllers/CouponController.cs
+++ Mango.Web/Controllers/CouponController.cs
@@ -23,5 +23,26 @@ namespace Mango.Web.Controllers
             } 
             return View(list);
         }
+
+        public async Task<IActionResult> CouponCreate()
+        {
+           
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponCreate(CouponDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                ResponseDto? response = await _couponService.CreateCouponsAsync(model);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(model);
+        }
+
     }
 }
