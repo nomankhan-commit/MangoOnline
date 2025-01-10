c76a1528271379a359cff068fe4938105c5fb567
NOMI NOMI <nhr146@outlook.com>
2025-01-10 17:30:58 +0500

    Consume login and registration apis  web application.

--- Mango.Web/Controllers/AuthController.cs
+++ Mango.Web/Controllers/AuthController.cs
@@ -1,7 +1,10 @@
 ﻿using Mango.Web.Models;
 using Mango.Web.Service.IService;
+using Mango.Web.Utility;
 using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 
 namespace Mango.Web.Controllers
 {
@@ -23,6 +26,7 @@ namespace Mango.Web.Controllers
         [HttpGet]
         public async Task<IActionResult> Register()
         {
+            InItRoleDDl();
             RegistrationRequestDto registrationRequest = new();
             return View(registrationRequest);
         }
@@ -32,6 +36,63 @@ namespace Mango.Web.Controllers
         {
             RegistrationRequestDto registrationRequest = new();
             return View(registrationRequest);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegistrationRequestDto obj)
+        {
+            ResponseDto result = await _authService.RegisterAsync(obj);
+
+            if (result != null && result.IsSuccess)
+            {
+                ResponseDto assignRole;
+                if (string.IsNullOrEmpty(obj.Role))
+                {
+                    obj.Role = SD.RoleCustomer;
+                }
+                assignRole = await _authService.AssingRoleAsync(obj);
+                if (assignRole != null && assignRole.IsSuccess)
+                {
+                    TempData["success"] = "Registration Successful";
+                    return RedirectToAction(nameof(Login));
+                }
+            }
+            else
+            {
+                TempData["error"] = result.Message;
+            }
+            InItRoleDDl();
+            return View(obj);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginRequestDto obj)
+        {
+            ResponseDto result = await _authService.LoginAsync(obj);
+
+            if (result != null && result.IsSuccess)
+            {
+                LoginResponseDto loginResponseDto = JsonConvert.
+                    DeserializeObject<LoginResponseDto>(Convert.ToString(result.Result));
+                
+                TempData["success"] = "Success!";
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                TempData["error"] = result.Message;
+                ModelState.AddModelError("CustomError", result.Message);
+                return View(obj);
+            }
+        }
+
+        void InItRoleDDl()
+        {
+            var roleList = new List<SelectListItem>() {
+                new SelectListItem {Text = SD.RoleAdmin, Value = SD.RoleAdmin },
+                new SelectListItem {Text = SD.RoleCustomer, Value = SD.RoleCustomer },
+            };
+            ViewBag.RoleList = roleList;
         }
     }
 }
