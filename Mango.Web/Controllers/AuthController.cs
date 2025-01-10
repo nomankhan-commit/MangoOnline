4085980913ae73d0aa566f7007a2d448d1a3da6e
NOMI NOMI <nhr146@outlook.com>
2025-01-10 17:13:04 +0500

    Create login and register page.

---
+++ Mango.Web/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            LoginRequestDto loginRequestDto = new();
+            return View(loginRequestDto);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            RegistrationRequestDto registrationRequest = new();
+            return View(registrationRequest);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            RegistrationRequestDto registrationRequest = new();
+            return View(registrationRequest);
+        }
+    }
+}
