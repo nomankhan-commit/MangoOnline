36dc05f5b657be918b58654b36c59b530e081ac9
NOMI NOMI <nhr146@outlook.com>
2025-01-11 06:48:13 +0500

    Create product api project and created product apis

---
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,123 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.Data;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+   // [Authorize]
+    public class ProductAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private ResponseDto _responseDto;
+        private IMapper _mapper;
+        public ProductAPIController(AppDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _responseDto = new ResponseDto();
+            _mapper = mapper;
+        }
+        
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                 IEnumerable<Product> obj = _db.Products.ToList();
+                 _responseDto.Result = _mapper.Map<IEnumerable<ProductDto>>(obj);
+            }
+            catch (Exception ex)
+            {
+
+                _responseDto.IsSuccess = false;
+                _responseDto.Result = ex.Message;
+
+            }
+            return _responseDto;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public object Get(int id) {
+
+            try
+            {
+                Product obj = _db.Products.First(u=>u.ProductId==id);
+                _responseDto.Result = _mapper.Map<ProductDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Result = ex.Message;
+            }
+            return _responseDto;
+        }
+
+        
+
+        [HttpPost]
+        [Authorize(Roles = "ADMIN")]
+        public object Post([FromBody] ProductDto productDto)
+        {
+
+            try
+            {
+                Product coupon = _mapper.Map<Product>(productDto);
+                _db.Products.Add(coupon);
+                _db.SaveChanges();
+                _responseDto.Result = _mapper.Map<ProductDto>(productDto);
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Result = ex.Message;
+            }
+            return _responseDto;
+        }
+
+        [HttpPut]
+        [Authorize(Roles = "ADMIN")]
+        public object Put([FromBody] ProductDto ProductDto)
+        {
+
+            try
+            {
+                Product coupon = _mapper.Map<Product>(ProductDto);
+                _db.Products.Update(coupon);
+                _db.SaveChanges();
+                _responseDto.Result = _mapper.Map<ProductDto>(ProductDto);
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Result = ex.Message;
+            }
+            return _responseDto;
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        [Authorize(Roles = "ADMIN")]
+        public object Delete(int id)
+        {
+
+            try
+            {
+                Product coupon = _db.Products.First(x => x.ProductId == id);
+                _db.Remove(coupon);
+                _db.SaveChanges();
+                _responseDto.Result = _mapper.Map<ProductDto>(coupon);
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Result = ex.Message;
+            }
+            return _responseDto;
+        }
+    }
+}
---
+++ Mango.Services.ProductAPI/Migrations/20250111014227_addproduct.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Mango.Services.ProductAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class addproduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "CategoryName", "Description", "ImageUrl", "Price", "ProductName" },
+                values: new object[,]
+                {
+                    { 1, "Appetizer", " Quisque vel lacus ac magna, vehicula sagittis ut non lacus.<br/> Vestibulum arcu turpis, maximus malesuada neque. Phasellus commodo cursus pretium.", "https://placehold.co/603x403", 15.0, "Samosa" },
+                    { 2, "Appetizer", " Quisque vel lacus ac magna, vehicula sagittis ut non lacus.<br/> Vestibulum arcu turpis, maximus malesuada neque. Phasellus commodo cursus pretium.", "https://placehold.co/602x402", 13.99, "Paneer Tikka" },
+                    { 3, "Dessert", " Quisque vel lacus ac magna, vehicula sagittis ut non lacus.<br/> Vestibulum arcu turpis, maximus malesuada neque. Phasellus commodo cursus pretium.", "https://placehold.co/601x401", 10.99, "Sweet Pie" },
+                    { 4, "Entree", " Quisque vel lacus ac magna, vehicula sagittis ut non lacus.<br/> Vestibulum arcu turpis, maximus malesuada neque. Phasellus commodo cursus pretium.", "https://placehold.co/600x400", 15.0, "Pav Bhaji" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
