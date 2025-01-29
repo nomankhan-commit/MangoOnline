383ce4d9aab9fe28f4f40c967f8377df688fe6be
NOMI NOMI <nhr146@outlook.com>
2025-01-29 06:21:03 +0500

    Create reward api project

---
+++ Mango.Services.RewardAPI/Program.cs
@@ -0,0 +1,46 @@
+using Mango.Services.RewardAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+ApplyMigration();
+app.Run();
+
+void ApplyMigration()
+{
+
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        if (_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+
+}
