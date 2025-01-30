7380d85f5de3cd45437e729e115c2957599ba53c
NOMI NOMI <nhr146@outlook.com>
2025-01-30 16:19:06 +0500

    Create Reward Service

---
+++ Mango.Services.RewardAPI/Services/IRewardService.cs
@@ -0,0 +1,10 @@
+﻿using Mango.Services.RewardAPI.Message;
+using Mango.Services.RewardAPI.Models;
+
+namespace Mango.Services.RewardAPI.Services
+{
+    public interface IRewardService
+    {
+        Task UpdateRewards(RewardsMessage rewardsMessage);
+    }
+}
---
+++ Mango.Services.RewardAPI/Services/RewardService.cs
@@ -0,0 +1,43 @@
+﻿using Mango.Services.RewardAPI.Data;
+using Mango.Services.RewardAPI.Message;
+using Mango.Services.RewardAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+
+namespace Mango.Services.RewardAPI.Services
+{
+    public class RewardService : IRewardService
+    {
+        private DbContextOptions<AppDbContext> options;
+        public RewardService(DbContextOptions<AppDbContext> options)
+        {
+            this.options = options;
+        }
+
+        
+        public async Task UpdateRewards(RewardsMessage rewardsMessage)
+        {
+            try
+            {
+                Rewards rewards = new()
+                {
+                    OrderId = rewardsMessage.OrderId,
+                    RewardActivity = rewardsMessage.RewardActivity,
+                    UserId = rewardsMessage.UserId,
+                    RewardDate = DateTime.Now,
+                };
+
+                await using var db = new AppDbContext(options);
+                db.Add(rewards);
+                await db.SaveChangesAsync();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+       
+
+    }
+}
