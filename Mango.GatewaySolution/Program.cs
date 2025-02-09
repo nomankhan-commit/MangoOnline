81041514eb8b39f03126501d78a87a8fca6f09dc
NOMI NOMI <nhr146@outlook.com>
2025-02-09 21:42:02 +0500

    Implement ocelot in product api.

--- Mango.GatewaySolution/Program.cs
+++ Mango.GatewaySolution/Program.cs
@@ -1,13 +1,14 @@
+using Mango.GatewaySolution.Extension;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Values;
 
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddOcelot();
+builder.Configuration.AddJsonFile("ocelot.json", optional:false,reloadOnChange:true);
+builder.AddAppAuthentication();
+builder.Services.AddOcelot(builder.Configuration);
 var app = builder.Build();
-
-
 app.MapGet("/", () => "Hello World!");
-app.UseOcelot();
+await app.UseOcelot();
 app.Run();
