f86b3ff860f03f5f8fec444a52fe4d0327bd644d
NOMI NOMI <nhr146@outlook.com>
2025-02-10 02:46:06 +0500

    Add       [Authorize] in order index.

--- Mango.Web/Controllers/OrderController.cs
+++ Mango.Web/Controllers/OrderController.cs
@@ -2,6 +2,7 @@
 using Mango.Web.Models;
 using Mango.Web.Service.IService;
 using Mango.Web.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Collections.Generic;
@@ -17,6 +18,7 @@ namespace Mango.Web.Controllers
             _orderService = orderService;
         }
 
+        [Authorize]
         public IActionResult Index()
         {
            
@@ -58,6 +60,8 @@ namespace Mango.Web.Controllers
             }
             return Json(new { data = list });
         }
+
+        [Authorize]
         public async Task<IActionResult> OrderDetail(int orderId)
         {
             OrderHeaderDto orderHeaderDto = new OrderHeaderDto();
